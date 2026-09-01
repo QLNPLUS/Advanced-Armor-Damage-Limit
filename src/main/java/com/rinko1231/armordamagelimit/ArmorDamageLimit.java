909ef4d50b471a06b54ec3d2efe688a63920dba8
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-09-01 15:16:54 +0800

    Add forge-1.19.2 standalone project

--- src/main/java/com/rinko1231/armordamagelimit/ArmorDamageLimit.java
+++ src/main/java/com/rinko1231/armordamagelimit/ArmorDamageLimit.java
@@ -1,16 +1,13 @@
 package com.rinko1231.armordamagelimit;
 
 import com.rinko1231.armordamagelimit.config.ArmorProtectionConfig;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(ArmorDamageLimit.MOD_ID)
-public class ArmorDamageLimit {
+public final class ArmorDamageLimit {
     public static final String MOD_ID = "advancedarmordamagelimit";
 
     public ArmorDamageLimit() {
-        MinecraftForge.EVENT_BUS.register(this);
         ArmorProtectionConfig.setup();
     }
-
 }
