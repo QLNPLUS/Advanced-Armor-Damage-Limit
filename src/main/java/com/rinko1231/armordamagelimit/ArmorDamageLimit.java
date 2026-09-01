20cd58920d60ac48f0d87145795349baff08fc30
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-09-01 15:17:15 +0800

    Add neoforge-1.21.1 standalone project

--- src/main/java/com/rinko1231/armordamagelimit/ArmorDamageLimit.java
+++ src/main/java/com/rinko1231/armordamagelimit/ArmorDamageLimit.java
@@ -1,16 +1,16 @@
 package com.rinko1231.armordamagelimit;
 
 import com.rinko1231.armordamagelimit.config.ArmorProtectionConfig;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
 
 @Mod(ArmorDamageLimit.MOD_ID)
-public class ArmorDamageLimit {
+public final class ArmorDamageLimit {
     public static final String MOD_ID = "advancedarmordamagelimit";
 
-    public ArmorDamageLimit() {
-        MinecraftForge.EVENT_BUS.register(this);
-        ArmorProtectionConfig.setup();
+    public ArmorDamageLimit(IEventBus modEventBus, ModContainer modContainer) {
+        modContainer.registerConfig(net.neoforged.fml.config.ModConfig.Type.COMMON,
+                ArmorProtectionConfig.SPEC, "AdvancedArmorDamageLimit.toml");
     }
-
 }
